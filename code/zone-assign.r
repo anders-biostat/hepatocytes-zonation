5f1b252d0b78b9f7e809488da1ebffb2d998d036
Alexey <alexey.mipt@gmail.com>
2020-07-10 12:07:09 +0200

    move functions

--- code/zone-assign.r
+++ code/zone-assign.r
@@ -4,36 +4,6 @@ library(dplyr)
 library(tidyr)
 source("code/func.r")
 
-## Here we reproduce Itykovitz 2017/2018 algo
-etaDensity <- function(eta, zone, gammaParams) {
-  shape <- gammaParams[zone, 1]
-  scale <- gammaParams[zone, 2]
-  dgamma(eta, shape = shape, scale = scale)
-}
-
-calcEta <- function(x) {
-  res <- x$portal / (x$portal + x$central)
-  normEta(res)
-}
-
-normEta <- function(x) {
-  (x - min(x, na.rm = TRUE)) / (max(x, na.rm = TRUE) - min(x, na.rm = TRUE))
-}
-
-calcEtaLikelihoods <- function(eta) {
-  zones <- 1:8
-  do.call(cbind, map(zones, ~ etaDensity(eta, .x)))
-}
-
-getPriors <- function(n) {
-  cellnums <- 2 * seq(1, n)
-  cellnums / sum(cellnums)
-}
-
-calcPosteriors <- function(zoneLiks, priors) {
-  x <- t(t(zoneLiks) * priors)
-  x / rowSums(x)
-}
 
 {cat("reading...")
   rdsDir <- file.path("results", "rds")
