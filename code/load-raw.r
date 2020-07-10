b0cef1472638fc78133127cf852cd468d2b7178f
Alexey <alexey.mipt@gmail.com>
2020-07-10 12:07:20 +0200

    fix problematic names

--- code/load-raw.r
+++ code/load-raw.r
@@ -1,15 +1,24 @@
+## This file reads raw counts into a single table and creates meta table for cells.
+## There were  seurat objects provided with clustering information,
+## no source code for that, but it looks like some cells were filtered out,
+## cell identities can not be mapped back to batches in obvious way, only guessing
+## from gene expression, cell number and condition.
 library(purrr)
 library(Matrix)
 meta <- read.table(
   "from_Ki/Ki experimental design.txt", sep = "\t",
   header = TRUE)
-saveRDS(meta, file.path("results", "rds", "meta.rds"))
+meta$ID <- sub(".+?_", "", meta$ID, perl=TRUE)
 
 countDir <- "from_Ki/Raw data/"
 
+## there two files with _2 suffix in filename. Ki's said, he made a mistake
+## and one needs to use the _2 files, as in meta.
 x <- list.files("from_Ki/Raw data/", recursive = TRUE) %>%
   strsplit(.Platform$file.sep)
 x <- Filter(function(a) grepl("csv$", a[[2]]), x)
+ids <- sub(".+?_", "", map_chr(x, 2), perl=TRUE) %>% sub(".csv", "", x = .)
+x <- x[ids %in% meta$ID]
 
 counts <- purrr::map(x, ~ read.csv(file.path(countDir, .x[1], .x[2])))
 
@@ -30,10 +39,13 @@ allcounts <- do.call(cbind, sparseCounts)
 saveRDS(allcounts, file.path("results", "rds", "allcouts.rds"))
 
 
-barcodes <- gsub("Batch", "", colnames(counts))
+meta$ID <- sub("_.+", "", meta$ID, perl=TRUE)
+barcodes <- gsub("Batch", "", colnames(allcounts))
 cellanno <- data.frame(
   barcode = barcodes,
   ID = gsub("_Cell.+", "", barcodes),
   stringsAsFactors = FALSE)
 cellanno <- cellanno %>% left_join(meta)
 saveRDS(cellanno, file.path("results", "rds", "cellanno.rds"))
+
+saveRDS(meta, file.path("results", "rds", "meta.rds"))
