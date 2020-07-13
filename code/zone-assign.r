8559559b981c60de4c2c8e2b9d444c187ea611c8
Alexey <alexey.mipt@gmail.com>
2020-07-13 16:53:26 +0200

    heatmaps

--- code/zone-assign.r
+++ code/zone-assign.r
@@ -28,7 +28,6 @@ setdiff(unlist(markers$itzkevitz), rownames(counts))
 ## use only genes we have
 markers$itzkevitz <- lapply(markers$itzkevitz, function(a) intersect(a, rownames(counts)))
 
-
 ## select condition
 sets <- list(
   "HEP-WT"  = cellanno$Cell.type == "HEP"  & cellanno$Genotype != "DoubleKO",
@@ -42,25 +41,67 @@ for(i in names(sets)[1:2]) {
   ## plot expression along eta and smoothing
   q <- d %>%
     ggplot(data= .) +
-    geom_smooth(aes(x = log(eta), y = frac), method = "loess") +
+    geom_smooth(aes(x = rank(eta), y = frac), method = "loess") +
     facet_wrap(~gene, scales = "free_y")
   ggsave(filename = figpath(sprintf("marker-frac-smooth-eta-%s.png", i)),
     q, width = 12, height = 8, dpi = 200)
 
   q <- d %>%
     ggplot(data= .) +
-    geom_smooth(aes(x = log(eta), y = frac), method = "loess") +
-    geom_point(aes(x = log(eta), y = frac), shape = ".") +
+    geom_smooth(aes(x = rank(eta), y = frac), method = "loess") +
+    geom_point(aes(x = rank(eta), y = frac), shape = ".") +
     facet_wrap(~gene, scales = "free_y")
   ggsave(filename = figpath(sprintf("marker-frac-smooth-points-eta-%s.png", i)),
     q, width = 12, height = 8, dpi = 200)
 
   q <- d %>%
     ggplot(data= .) +
-    geom_point(aes(x = (eta), y = frac*total), shape = ".") +
+    geom_point(aes(x = rank(eta), y = frac*total), shape = ".") +
     facet_wrap(~gene, scales = "free")
 
   ggsave(filename = figpath(sprintf("marker-count-vs-eta-%s.png", i)),
     q, width = 12, height = 8, dpi = 200)
+
+}
+
+
+averageGenesOverZones <- function(d, zoneNum) {
+  x <- d %>%
+    mutate(etaq = rank(eta)/length(eta), zone = cut(etaq, 0:zoneNum/zoneNum))
+  x <- x %>% group_by(gene, zone) %>%
+    summarise(frac = mean(frac)) %>%
+    ungroup()
+  x <- x %>% pivot_wider(names_from = gene, values_from = frac)
+
+  m <- as.matrix(x[, -1])
+  rownames(m) <- x$zone
+  t(m)
+}
+
+
+for(i in names(sets)[1:2]) {
+d <- makeEtaDF(sets[[i]], cellanno, counts, fracs, markers$itzkevitz)
+m <- averageGenesOverZones(d, zoneNum = 6)
+
+pheatmap(
+  m,
+  scale = "row",
+  cluster_cols = FALSE,
+  cluster_rows = TRUE)
+
+fn <- figpath(sprintf("marker-heatmap-zones-%s.png", i))
+
+geneType <- data.frame(
+  marker = c("central", "portal")[1 + rownames(m) %in% markers$itzkevitz$central])
+rownames(geneType) <- rownames(m)
+
+pheatmap(
+  filename = fn,
+  annotation_row = geneType,
+  m,
+  scale = "row",
+  cluster_cols = FALSE,
+  cluster_rows = TRUE,
+  width = 10, height = 8, res = 200)
 
 }
