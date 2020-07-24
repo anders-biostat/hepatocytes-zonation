c602accf1ea83d15ba00f1ea1201d5ddea5de801
Alexey <alexey.mipt@gmail.com>
2020-07-24 15:22:23 +0200

    pbulks

---
+++ code/235-pbulk-de-conditions.r
@@ -0,0 +1,45 @@
+library(DESeq2)
+library(Matrix)
+library(purrr)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(pheatmap)
+source("code/func.r")
+source("code/assets.r")
+
+figdir <- file.path("results", "figs", "zonation")
+
+makeDE <- function(counts, cellanno) {
+  m <- model.matrix(~ 0 + sample, data = cellanno)
+  pbulks <- counts %*% m
+  pbulks <- pbulks[rowMeans(pbulks) > .5, ]
+  ## "sampleLSEC|Wildtype 3 9" -> "LSEC|Wildtype 3 9" 
+  colnames(pbulks) <- gsub("sample", "", colnames(pbulks))
+  bulkanno <- unique(cellanno[c("Genotype", "sample")])
+  rownames(bulkanno) <- bulkanno$sample
+  pbulks <- pbulks[, rownames(bulkanno)]
+
+  dds <- DESeq2::DESeqDataSetFromMatrix(pbulks, colData = bulkanno, design = ~ Genotype)
+  dds <- DESeq2::DESeq(dds)
+  res <- DESeq2::results(dds)
+
+  res <- res %>% as.data.frame %>% arrange(pvalue)
+  list(result = res, pbulks = pbulks, dds = dds, anno = bulkanno)
+}
+
+read <- function(cellType) {
+  cat("reading...", cellType)
+  counts <- loadFiles(glue::glue("counts-{cellType}"))
+  cellanno <- loadFiles(glue::glue("cellanno-{cellType}"))
+  cellanno$sample <- paste(cellanno$condition, cellanno$Mouse.ID, cellanno$Experimental.Batch)
+  cat("OK\n")
+  list(cellanno = cellanno, counts = counts)
+}
+
+celltypes <- c("heps", "lsec") %>% set_names
+dat <- lapply(celltypes, read)
+results <- lapply(dat, do.call, what = makeDE)
+
+saveRDS(results, file.path(rdsDir, "pbulks-deseq.rds"))
+
