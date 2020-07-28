e1abfb7360b2050efbf33dbe3ec31f83006b8215
Alexey <alexey.mipt@gmail.com>
2020-07-28 15:32:34 +0200

    mup genes plot

--- code/221-seurat-select-heps.r
+++ code/221-seurat-select-heps.r
@@ -15,8 +15,8 @@ heps <- s$HEP
 inuse <- colnames(heps)[heps@meta.data$seurat_clusters != 4]
 
 mtpercent <- getGenePercent(counts, totals)
-
-d <- cbind(cellanno, mt = mtpercent, total = totals)
+muppercent <- getGenePercent(counts, totals, "^mup")
+d <- cbind(cellanno, mt = mtpercent, total = totals, mup = muppercent)
 q <- ggplot(data = d %>% filter(Cell.type == "HEP")) +
   geom_point(aes(y = mt, x = total, colour = barcode %in% inuse), size = .5) +
   scale_x_log10() +
@@ -24,5 +24,12 @@ q <- ggplot(data = d %>% filter(Cell.type == "HEP")) +
  facet_wrap(~ sample, scales = "free_x")
 ggsave(figpattern("selected-heps-mito-percent.png"), q, width = 10, height = 9, dpi = 200)
 
+
+q <- ggplot(data = d %>% filter(Cell.type == "HEP")) +
+  geom_point(aes(y = mup, x = total, colour = barcode %in% inuse), size = .5) +
+  scale_x_log10() +
+  scale_colour_discrete(name = "selected") +
+  facet_wrap(~ sample, scales = "free_x")
+ggsave(figpattern("selected-heps-mup-percent.png"), q, width = 10, height = 9, dpi = 200)
 
 saveRDS(inuse, file.path(rdsDir, "selected-heps.rds"))
