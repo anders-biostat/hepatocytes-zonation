d06e0eba9173047c9cf5b995b0f470d961bb36eb
Alexey <alexey.mipt@gmail.com>
2020-07-30 11:02:17 +0200

    more corrections

--- code/245-correction-of-gene-names.r
+++ code/245-correction-of-gene-names.r
@@ -23,7 +23,7 @@ xl2lists <- function(x) {
   fracs <- t(t(counts) / totals)
   markers <- loadFiles("markers")$itzkevitz
   markers <- add6LandMarks(markers)
-  OK()
+  cat("OK\n")
 }
 
 shalevList <- read_excel(geneFile, sheet = 1)
@@ -64,24 +64,57 @@ correctGenes <- function(x, corrmap) {
 
 shalevList <- map(shalevList, correctGenes, alias2symbol)
 hepatoList <- map(hepatoList, correctGenes, alias2symbol)
+
+## read corrections from Ki
+kilist <- unlist(yaml::read_yaml(file.path("from_Ki", "gene-aliases-ki.yaml")))
+shalevList <- map(shalevList, correctGenes, kilist)
+hepatoList <- map(hepatoList, correctGenes, kilist)
+
 allgenes <- unique(c(unlist(shalevList), unlist(hepatoList)))
 a <- setdiff(allgenes, rownames(counts))
+## still 32 genes not found, try to recover using biomart
 
-x <- map(a, ~ agrep(.x, x = rownames(counts), max.distance = 1, value = TRUE))
-names(x) <- a
-map_int(x, length) %>% sort 
+library(biomaRt)
+ensembl <- useMart("ensembl", dataset = "mmusculus_gene_ensembl")
+symbolattrs <- c("external_gene_name", "hgnc_symbol", "mgi_symbol", "uniprot_gn_symbol",
+  "external_synonym")
+
+martsymbols <- map(symbolattrs[5], ~getBM(
+  attributes = symbolattrs,
+  filters = .x,
+  values = a,
+  mart = ensembl))
+
+x <- martsymbols[[1]]
+x <- unique(x[c("mgi_symbol", "external_synonym")])
+x <- map_df(x, tolower)
+i <- x$mgi_symbol %in% rownames(counts)
+
+alias2symbol <- set_names(x$mgi_symbol[i], x$external_synonym[i])
+## there are some ambiguous pairs (asp, cs1)
+## 1700052n19rik: armt1
+## 2700094k13rik: selenoh
+## 4930420k17rik: tmem243
+## 4933433p14rik: gskip
+## 8430408g22rik: depp1
+## 8430410k20rik: msantd4
+## asp: ropn1l
+## asp: a
+## asp: tmprss11d
+## c230081a13rik: peak1
+## cs1: itprid2
+## cs1: slamf7
+## lat1: slc7a5
+## mrp4: abcc4
+## pox1: prodh2
+length(setdiff(a, names(alias2symbol)))
+## 20 genes still missing
+alias2symbol <- alias2symbol[! alias2symbol %in% c("a", "tmprss11d", "itprid2")]
+
+shalevList <- map(shalevList, correctGenes, alias2symbol)
+hepatoList <- map(hepatoList, correctGenes, alias2symbol)
 
 shalevList <- map(shalevList, ~ .x[.x %in% rownames(counts)])
 hepatoList <- map(hepatoList, ~ .x[.x %in% rownames(counts)])
 saveRDS(list(shalev = shalevList, ki = hepatoList),
-        file.path(rdsDir, "functional-genes.rds"))
-
-library(biomaRt)
-ensembl <- useMart("ensembl", dataset = "mmusculus_gene_ensembl")
-symbolattrs <- c("external_gene_name", "hgnc_symbol", "mgi_symbol", "uniprot_gn_symbol")
-
-martsymbols <- map(symbolattrs[1], ~getBM(
-  attributes = symbolattrs,
-  filters = .x,
-  values = corrections[!success,]$SYMBOL,
-  mart = ensembl))
+  file.path(rdsDir, "functional-genes.rds"))
