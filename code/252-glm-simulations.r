2f6a01b490e50a2f424d1b0d4ff2e49ffaafc08d
Alexey <alexey.mipt@gmail.com>
2020-10-07 17:50:48 +0200

    simulate

--- code/252-glm-simulations.r
+++ code/252-glm-simulations.r
@@ -75,7 +75,7 @@ modelform <- as.formula(
   sprintf("gene ~ -1 + offset(log(total)) + (%s):mouse",
     paste0(splineVars, collapse = "+")))
 
-qs <- map(rownames(hepde)[1:10], function(g) {
+qs <- map(rownames(hepde)[1:20], function(g) {
   try({
   m <- glm.nb(modelform, data = cbind(gene = counts[g,], mat))
   pred <- predict(m, newdata = Xpred)
@@ -91,6 +91,7 @@ ggsave(filename = file.path(figdir, "fit-per-mouse.png"),
 
 ## take a gene with prominent difference and counts
 g <- "gulo"
+## g <- "cyp2c37"
 ## model pro mouse and model pro condition
 modelform <- as.formula(
   sprintf("gene ~ -1 + offset(log(total)) + (%s):mouse",
@@ -117,6 +118,7 @@ l <- list(mixed = mmixed, condition = mcondition, mouse = mmouse)
 ## Indeed, using a simpler model with conditions only may result in more
 ## false positives, since the variance is smaller and does not account for
 ## biological variation.
+
 # fixed effects pro condition
 ##                            Estimate Std. Error   z value      Pr(>|z|)
 ## X1:conditionHEP|Wildtype  -7.109897  0.1111517 -63.96572  0.000000e+00
@@ -131,7 +133,6 @@ l <- list(mixed = mmixed, condition = mcondition, mouse = mmouse)
 ## X5:conditionHEP|DoubleKO -12.940257  0.6496367 -19.91922  2.772610e-88
 
 # mixed effect model
-## > a1
 ##                            Estimate Std. Error   z value      Pr(>|z|)
 ## X1:conditionHEP|Wildtype  -7.103366  0.1256707 -56.52366  0.000000e+00
 ## X1:conditionHEP|DoubleKO  -7.926666  0.1613386 -49.13064  0.000000e+00
@@ -143,6 +144,14 @@ l <- list(mixed = mmixed, condition = mcondition, mouse = mmouse)
 ## conditionHEP|DoubleKO:X4 -12.856112  1.0833392 -11.86712  1.754105e-32
 ## conditionHEP|Wildtype:X5 -11.413403  0.2861650 -39.88399  0.000000e+00
 ## conditionHEP|DoubleKO:X5 -12.977334  0.6527144 -19.88210  5.814484e-88
+
+## Random effects:
+##          Groups  Name Variance  Std.Dev. 
+## mouse   X1   1.310e-02 1.145e-01
+## mouse.1 X2   1.509e-01 3.885e-01
+## mouse.2 X3   1.322e-01 3.636e-01
+## mouse.3 X4   2.523e-11 5.023e-06
+## mouse.4 X5   1.879e-10 1.371e-05
 
 
 q <- qplot(
@@ -157,7 +166,37 @@ q <- qplot(
 ggsave(filename = file.path(figdir, sprintf("%s-mixed-prediction.png", g)), q,
   width = 5, height = 4, dpi = 200)
 
+
+## simulate and estimate
+
 modelform <- as.formula(
-  paste("sqrt(gene/total) ~ -1 +   ",
-    paste0(sprintf("%s:condition +(0+%s|mouse)", splineVars, splineVars), collapse = "+")))
-mmixedfrac <- lme4::lmer(modelform, data = cbind(gene = counts[g,], mat))
+  sprintf("gene ~ -1 + offset(log(total)) + (%s):condition",
+    paste0(splineVars, collapse = "+")))
+
+x <- replicate(100,  {
+  y <- simulate(mmixed)[,1]
+  m <- glm.nb(modelform, data = cbind(gene = y, mat))
+  list(list(coef = coef(m), vcov = vcov(m)))
+})
+
+do.call(rbind, map(x, "coef")) %>%
+  apply(2, sd)
+coef(summary(mmixed))
+
+cbind(coef(summary(mmixed)),
+simvar = do.call(rbind, map(x, "vcov") %>% map(diag)) %>% sqrt %>%
+  apply(2, mean)
+)
+
+
+##                            Estimate Std. Error   z value      Pr(>|z|)    simvar
+## X1:conditionHEP|Wildtype  -7.103366  0.1256707 -56.52366  0.000000e+00 0.1070544
+## X1:conditionHEP|DoubleKO  -7.926666  0.1613386 -49.13064  0.000000e+00 0.1468570
+## conditionHEP|Wildtype:X2  -9.348529  0.4174393 -22.39494 4.408787e-111 0.3492303
+## conditionHEP|DoubleKO:X2 -10.133175  0.5549899 -18.25830  1.777282e-74 0.5167947
+## conditionHEP|Wildtype:X3  -7.981858  0.6627147 -12.04418  2.081273e-33 0.6252606
+## conditionHEP|DoubleKO:X3 -10.723040  1.0239517 -10.47221  1.159020e-25 1.0123840
+## conditionHEP|Wildtype:X4 -13.776912  0.6080382 -22.65797 1.164234e-113 0.6052846
+## conditionHEP|DoubleKO:X4 -12.856112  1.0833392 -11.86712  1.754105e-32 1.1107144
+## conditionHEP|Wildtype:X5 -11.413403  0.2861650 -39.88399  0.000000e+00 0.2821492
+## conditionHEP|DoubleKO:X5 -12.977334  0.6527144 -19.88210  5.814484e-88 0.6889879
