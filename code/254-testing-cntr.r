f9271420b4d1a4e4c2d938dad856d5679af82cd5
Alexey <alexey.mipt@gmail.com>
2020-11-11 14:15:43 +0100

    rm comment

--- code/254-testing-cntr.r
+++ code/254-testing-cntr.r
@@ -47,7 +47,6 @@ crossgroupVar <- function(beta, positions, X, wildmice, komice) {
   colmouse <- gsub(".+mouse", "", names(beta))
   betamat <- matrix(beta, ncol = ncol(x))
   rownames(betamat) <- colmouse[seq_len(nrow(betamat))]
-  ## divide by n^2 since it is var of mean for mice groups
   vwild <- apply(betamat[wildmice,], 2, var)/ (length(wildmice)-1)
   vko <- apply(betamat[komice,], 2, var) / (length(komice)-1)
   ## shortcut instead of creating diagonal vcov matrix
